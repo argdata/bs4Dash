06e8d6487fea3b0f524a224b283a6d656ef65d85
Divad0175 <dgranjon@ymail.com>
2019-01-22 13:10:52 +0100

    put js in the dashboardBody

--- R/dashboardBody.R
+++ R/dashboardBody.R
@@ -10,6 +10,22 @@
 bs4DashBody <- function(...) {
   shiny::tags$div(
     class = "content-wrapper",
+    shiny::tags$head(
+      shiny::tags$script(
+        "// handle shinyapps.io: we need to extract the worker id and
+         // paste it in the url so that the apps works correctly
+         // get the shiny app.io workerId
+         var workerId = $('base').attr('href');
+         // ensure that this code does not run on shiny server/pro and locally
+         if (typeof workerId != 'undefined') {
+           // get the initial page url
+           var url = window.location.href;
+           // replace the url by the url for shinyapp.io
+           window.location.replace(url + workerId);
+         }
+        "
+      )
+    ),
     ...
   )
 }
--- R/deps.R
+++ R/deps.R
@@ -15,7 +15,6 @@ addDeps <- function(x, theme) {
   # put all necessary ressources here
   adminLTE3_js <- "adminlte.js"
   bs4Dash_js <- "bs4Dash.js"
-  relocate_js <- "relocate.js"
   adminLTE3_css <- "adminlte.min.css"
   jquery_ui_js <- "jquery-ui.min.js"
   bootstrap_js <- "bootstrap.bundle.min.js"
@@ -52,7 +51,7 @@ addDeps <- function(x, theme) {
       name = "bs4Dash",
       version = as.character(utils::packageVersion("bs4Dash")),
       src = c(file = system.file("bs4Dash-0.2.0", package = "bs4Dash")),
-      script = c(bs4Dash_js, relocate_js)
+      script = bs4Dash_js
     ),
     # fontawesome
     htmltools::htmlDependency(
