32085a887507c46d42902860bc6109458e79c836
Divad0175 <dgranjon@ymail.com>
2019-01-22 13:21:56 +0100

    load first

--- inst/bs4Dash-0.2.0/bs4Dash.js
+++ inst/bs4Dash-0.2.0/bs4Dash.js
@@ -1,5 +1,20 @@
 $(function () {
   
+  // handle shinyapps.io, RStudio connect and shiny server pro: 
+  // we need to extract the worker id and
+  // paste it in the url so that the apps works correctly
+  
+  // get the  workerId
+  var workerId = $('base').attr('href');
+  // ensure that this code does not run on shiny server/pro and locally
+  if (typeof workerId != 'undefined') {
+    // get the initial page url
+    var url = window.location.href;
+    // replace the url by the url for shinyapp.io
+    window.location.replace(url + workerId);
+  }
+  
+  
   // Make the dashboard widgets sortable Using jquery UI
   $('.connectedSortable').sortable({
     placeholder         : 'sort-highlight',
