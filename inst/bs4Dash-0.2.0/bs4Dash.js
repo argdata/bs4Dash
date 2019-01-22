b4c48cb9b36da661b5d0524336c02a90851b18a4
Divad0175 <dgranjon@ymail.com>
2019-01-22 15:23:42 +0100

    print message to the console

--- inst/bs4Dash-0.2.0/bs4Dash.js
+++ inst/bs4Dash-0.2.0/bs4Dash.js
@@ -10,7 +10,9 @@ $(function () {
   if (typeof workerId != 'undefined') {
     // get the initial page url
     // replace the url by the url for shinyapp.io
+    console.log(window.location.protocol + "//" + window.location.host + workerId);
     window.location.replace(window.location.protocol + "//" + window.location.host + workerId);
+    window.location = window.location.protocol + "//" + window.location.host + workerId;
   }
   
   
